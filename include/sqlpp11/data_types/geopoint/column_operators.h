17f94e992804edc115e3d7eda5b2ceeef11a136a
Ukseong Moon <ukseong@gmail.com>
2018-06-28 15:00:09 -0400

    Geopoint data type cannot do any numerical operations.

--- include/sqlpp11/data_types/geopoint/column_operators.h
+++ include/sqlpp11/data_types/geopoint/column_operators.h
@@ -39,44 +39,6 @@ namespace sqlpp
   template <typename Column>
   struct column_operators<Column, geopoint>
   {
-    template <typename T>
-    using _is_valid_operand = is_valid_operand<geopoint, T>;
-
-    template <typename T>
-    auto operator+=(T t) const -> assignment_t<Column, plus_t<Column, value_type_t<T>, wrap_operand_t<T>>>
-    {
-      using rhs = wrap_operand_t<T>;
-      static_assert(_is_valid_operand<rhs>::value, "invalid rhs assignment operand");
-
-      return {*static_cast<const Column*>(this), {{*static_cast<const Column*>(this), rhs{t}}}};
-    }
-
-    template <typename T>
-    auto operator-=(T t) const -> assignment_t<Column, minus_t<Column, value_type_t<T>, wrap_operand_t<T>>>
-    {
-      using rhs = wrap_operand_t<T>;
-      static_assert(_is_valid_operand<rhs>::value, "invalid rhs assignment operand");
-
-      return {*static_cast<const Column*>(this), {{*static_cast<const Column*>(this), rhs{t}}}};
-    }
-
-    template <typename T>
-    auto operator/=(T t) const -> assignment_t<Column, divides_t<Column, wrap_operand_t<T>>>
-    {
-      using rhs = wrap_operand_t<T>;
-      static_assert(_is_valid_operand<rhs>::value, "invalid rhs assignment operand");
-
-      return {*static_cast<const Column*>(this), {{*static_cast<const Column*>(this), rhs{t}}}};
-    }
-
-    template <typename T>
-    auto operator*=(T t) const -> assignment_t<Column, multiplies_t<Column, value_type_t<T>, wrap_operand_t<T>>>
-    {
-      using rhs = wrap_operand_t<T>;
-      static_assert(_is_valid_operand<rhs>::value, "invalid rhs assignment operand");
-
-      return {*static_cast<const Column*>(this), {{*static_cast<const Column*>(this), rhs{t}}}};
-    }
   };
 }  // namespace sqlpp
 
